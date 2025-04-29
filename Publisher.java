d184611fc172e7688a0be80501c23ba0d08ae83c
Felipe-Tapiaa <129549701+Felipe-Tapiaa@users.noreply.github.com>
2025-04-28 21:05:25 -0400

    Add files via upload

---
+++ Publisher.java
@@ -0,0 +1,14 @@
+public class Publisher extends Component {
+   protected Publisher() {} // to ban calls to this constructor
+   
+   public Publisher(String name, Broker broker, String topicName) {
+      super(name, topicName); 
+      this.topic = broker.createTopic(topicName);
+   }
+   protected void publishNewEvent(String message) {
+      if (topic != null) {
+         topic.notify(message); 
+     }
+   }
+   private Topic topic; 
+}
